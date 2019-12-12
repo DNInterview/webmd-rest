e931b656cab4d981503b436b9b12e1e94dfe9804
Daniel <dnomura@twiagemed.com>
2019-12-11 17:36:55 -0800

    updated start script

--- src/start.ts
+++ src/start.ts
@@ -1,15 +1,7 @@
 import express from 'express';
-
+import { ExpressApp } from './core/web-app/ExpressApp';
 
 const app = express();
-const port = 3000;
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
-});
-
-export default () => {
-
-}
+const expressApp = new ExpressApp(app);
+expressApp.setup();
+expressApp.start();
